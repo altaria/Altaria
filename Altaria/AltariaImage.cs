ae3eae2b01983a555fad0484c0c6cd43ff63de3d
causztic <limyaojie93@gmail.com>
2012-12-19 14:21:11 +0800

    + NewHaarTransform, + NewHaarRestore, + NewEmbedWatermark

--- Altaria/AltariaImage.cs
+++ Altaria/AltariaImage.cs
@@ -1,52 +1,165 @@
 ﻿using System;
+using System.Windows.Media.Imaging;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Drawing;
+using System.IO;
 
 namespace Altaria
 {
     public class AltariaImage
     {
-        //variables
-        ///<summary>The original bmp.</summary>
-        public Bitmap originalbmp    { get; private set; } 
-        /// <summary>
-        /// The transformed bmp after HaarTransform.
-        /// </summary>
-        public Bitmap transformedbmp { get; private set; }
-        public bool transformed = false;
-        /// <summary>
-        /// The dimensions of the originalbmp. transformedbmp should also have the same dimensions.
-        /// </summary>
+        private const double alpha = 0.2;
+        private const double c0 = 0.7071067;
+        private const double c1 = 0.7071067;
+       // public Bitmap originalbmp    { get; private set; } 
+       // public Bitmap transformedbmp { get; private set; }
         public Int32[] dimensions    { get; private set; }
+       // public Bitmap watermarkedbmp { get; private set; }
+        public bool is_transformed      { get; private set; }
+        public bool is_watermarked      { get; private set; }
+        public double[,] original       { get; private set; }
+        private double[,] placeholder;
+        public double[,] transformed    { get; private set; }
+        public string name              { get; private set; }
 
-        /// <summary>
-        /// The watermarkedbmp after EmbedWatermark
-        /// </summary>
-        public Bitmap watermarkedbmp { get; private set; }
-        public bool watermarked    { get; private set; }
-
-        //four subdomains of the transformed image by haar.
-        public string name { get; private set; }
-        //constructor
-        public AltariaImage(Bitmap bmp, string name)
+      /*  public AltariaImage(Bitmap bmp, string name)
         {
             this.originalbmp = bmp; //original image
             this.dimensions = new Int32[2]{ bmp.Height, bmp.Width };
             this.name = name;
             this.watermarked = false;
         }
-        
-/* The Haar Transform is performed in two stages. First, each row is transformed. Then each column is transformed. The coefficients
- * that store the "averages" are moved into the first portion of the image while the detail coefficients remain on the outer portion
- * of the image. Since the averaged version is on the top left, further transforms can be applied directly to that quarter of the
- * image. Hence the use of a scale variable to determine which portion of the image to transform. Note that the image stored in the 
- * Bitmap bmp should have dimensions that are a power of 2.
- * 
+      */
+        public AltariaImage(Stream filestream, string filename)
+        {
+            Bitmap bmp = new Bitmap(filestream);
+            filestream.Position = 0;
+            name = filename;
+            original = new double[bmp.Height, bmp.Width];
+            placeholder = new double[bmp.Height, bmp.Width];
+            transformed = new double[bmp.Height, bmp.Width];
+            dimensions = new Int32[2] { bmp.Height, bmp.Width };
+            for (int i = 0; i < bmp.Height; i++)
+                for (int j = 0; j < bmp.Width; j++)
+                    original[i,j] = filestream.ReadByte();
+            is_watermarked = false;
+        }
+
+        /// <summary>
+        /// Based on putmark.c
+        /// </summary>
+        /// <param name="level">The number of times to decompose.</param>
+        public void NewHaarTransform(int level)
+        {
+            transformed = original;
+            int ix, jy;
+            int L = original.GetLength(0);
+            int Lh = L / 2;
+            while (level > 0)
+            {
+                for (int j = 0; j < L; j++)
+                {
+                    for (int i = 0; i <= L - 1; i += 2)
+                    {
+                        ix = i >> 1;
+                        placeholder[ix, j] = c0 * transformed[i, j] + c1 * transformed[i + 1, j];
+                        placeholder[ix + Lh, j] = c0 * transformed[i, j] - c1 * transformed[i + 1, j];
+                    }
+                }
+                for (int i = 0; i < L; i++)
+                {
+                    for (int j = 0; j <= L - 1; j += 2)
+                    {
+                        jy = j >> 1;
+                        transformed[i, jy] = c0 * placeholder[i, j] + c1 * placeholder[i, j + 1];
+                        transformed[i, jy + Lh] = c0 * placeholder[i, j] - c1 * placeholder[i, j + 1];
+                    }
+                }
+                --level;
+                L = Lh;
+                Lh = L / 2;
+            }
+        }
+        /// <summary>
+        /// Based on putmark.c
+        /// </summary>
+        /// <param name="level">The number of times to decompose</param>
+        public void NewHaarRestore(int level)
+        {
+            int Lh = (int)(original.GetLength(0) / Math.Pow(2, level));
+            int L = Lh * 2;
+            int jy, ix;
+            while (level > 0)
+            {
+                for (int i = 0; i < L; i++)
+                    for (int j = 0; j < L; j += 2)
+                    {
+                        jy = j >> 1;
+                        placeholder[i, j] = transformed[i,jy];
+                        placeholder[i,j + 1] = transformed[i,jy + Lh];
+                    }
+                for (int i = 0; i < L; i++)
+                    for (int j = 0; j < L; j += 2)
+                    {
+                        transformed[i,j] = c0 * placeholder[i,j] + c0 * placeholder[i,j + 1];
+                        transformed[i,j + 1] = c1 * placeholder[i,j] - c1 * placeholder[i,j + 1];
+                    }
+                /* Y <-- (P^-1)*X,  X <-- W'*Y        */
+                for (int j = 0; j < L; j++)
+                    for (int i = 0; i < L; i += 2)
+                    {
+                        ix = i >> 1;
+                        placeholder[i,j] = transformed[ix,j];
+                        placeholder[i + 1,j] = transformed[ix + Lh,j];
+                    }
+                for (int j = 0; j < L; j++)
+                    for (int i = 0; i < L; i += 2)
+                    {
+                        transformed[i,j] = c0 * placeholder[i,j] + c0 * placeholder[i + 1,j];
+                        transformed[i + 1,j] = c1 * placeholder[i,j] - c1 * placeholder[i + 1,j];
+                    }
+                --level;
+                Lh = L;
+                L = Lh * 2;
+            }
+            byte[] data = new byte[transformed.Length];
+            for (int i = 0; i < transformed.GetLength(0); i++)
+                for (int j = 0; j < transformed.GetLength(1); j++)
+                    data[i+j] = (byte)transformed[i,j];
+            //convert the 2d array to image.
+            /*System.Windows.Media.PixelFormat pf = System.Windows.Media.PixelFormats.Bgr32;
+            int rawStride = (transformed.GetLength(1) * pf.BitsPerPixel + 7) / 8;
+            BitmapSource bms = BitmapSource.Create(dimensions[1], dimensions[0], 96, 96, pf, null, data, rawStride);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bms));
+                enc.Save(ms);
+                new Bitmap(ms).Save("C:\\temp\\watermarked.bmp");
+            }*/
+        }
+
+        public void NewEmbedWatermark(AltariaImage wm)
+        {
+            int n3 = original.GetLength(0) / 8;
+            int n2 = n3 * 2;
+            for (int i = 0; i < n3; i++)
+                for (int j = n3; j < n2; j++)
+                {
+                    transformed[i, j] *= (1 + alpha * wm.original[i, j - n3]);
+                }
+            for (int i = n3; i < n2; i++)
+                for (int j = 0; j < n3; j++)
+                {
+                    transformed[i, j] *= (1.0 - alpha * wm.original[i - n3, j]);
+                }
+            is_watermarked = true;
+        }
+
+/* 
  * http://books.google.com.sg/books?id=IGtIWmM2GWIC&pg=PA164&lpg=PA164&dq=c%23+haar+transform&source=bl&ots=eCFYSo3h7R&sig=PX_k3bV4emlrHclPqhUzKN6M9qU&hl=en&sa=X&ei=C2GwUJ_NA82mrAf65oHYBg&ved=0CCwQ6AEwAA#v=onepage&q=c%23%20haar%20transform&f=false
- *
- * *
  */
 
         /// <summary>
@@ -55,7 +168,7 @@ namespace Altaria
         /// <param name="bmp">The bitmap to transform. If none is provided, originalbmp will be used.</param>
         /// <param name="level">The number of times to perform Haar Transform.</param>
         /// <param name="scale">The starting scale. Defaults to 1.</param>
-        public void HaarTransform(Bitmap bmp, int level, int scale = 1)
+        /*public void HaarTransform(Bitmap bmp, int level, int scale = 1)
         {
             Bitmap originalbmp = bmp;
             if (bmp == null)
@@ -261,16 +374,17 @@ namespace Altaria
                 }
             }
             return reshaped_image;
-        }
+        }*/
 
+        /// <summary>
+        /// Not working yet.
+        /// </summary>
+        /// <param name="watermark"></param>
+        /*
         public void EmbedWatermark(AltariaImage watermark)
         {
                 Bitmap b = new Bitmap(this.transformedbmp);
                 Bitmap wm = new Bitmap(watermark.originalbmp);
-                //double alpha = 0.2;
-                /*
-                 * the below code is based on putmark.c
-                */
                 int n3 = b.Height / 8;
                 int n2 = n3 * 2;
                 int color = 0;
@@ -291,6 +405,8 @@ namespace Altaria
                 this.watermarked = true;
                 this.watermarkedbmp = b;
         }
+         */
+
         /// <summary>
         /// Function to obtain the Peak Signal-to-Noise Ratio. A higher value would normally indicate that the reconstruction
         /// is of a higher quality. It is an approximation to human perception of reconstruction quality.
@@ -374,6 +490,7 @@ namespace Altaria
             }
             
         }
+
     }
 
 }
